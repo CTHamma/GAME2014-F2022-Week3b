a84b8769251af39e7729404b70660d556a4954a8
CTHamma <khuynht2p@outlook.com>
2022-09-15 14:58:31 -0400

    added boundary

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -6,21 +6,22 @@ using UnityEngine.AI;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Boundary boundary;
+    public float veritcalPos;
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector2 oldPosition = transform.position;
-
-        transform.position = oldPosition + new Vector2(horizontal, vertical) * Time.deltaTime;
+        Move();
     }
+
+    public void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+
+        transform.position += new Vector3(x, 0, 0) * Time.deltaTime;
+        float clampedPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
+        transform.position = new Vector2(clampedPosition, veritcalPos);
+    }
+
 }
